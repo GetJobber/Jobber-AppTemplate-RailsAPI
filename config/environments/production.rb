59a3c75ad332633cf36adb6958134b68034579d1
Harry Jackson <upwardbinkie@gmail.com>
2022-06-02 12:59:22 -0500

    Add Docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Jobber_AppTemplate_RailsAPI_production"
+  # config.active_job.queue_name_prefix = "jobber_apptemplate_railsapi_production"
 
   config.action_mailer.perform_caching = false
 
