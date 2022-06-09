2cfc085db46d75ce3a1e93ef76369ccdc3ea86ff
Christian Prado <christianprado.contato@gmail.com>
2022-06-09 18:46:08 -0300

    Added clients controller tests

---
+++ spec/app/controllers/clients_controller_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe(ClientsController, type: :controller) do
+  let(:jobber_account) { create(:jobber_account) }
+  let(:result) { JSON.parse(response.body) }
+
+  before do
+    allow(controller).to(receive(:jobber_account_id).and_return(jobber_account.jobber_id))
+  end
+
+  describe "#index" do
+    context "when query is executed successfully" do
+      let(:clients_querry_result) do
+        [
+          {
+            "id" => "Z2lkOi8vSm9iYmVyL0NsaWVudC80NjQ0OTMyNw==",
+            "name" => "Arthas Menethil",
+          },
+          {
+            "id" => "Z2lkOi8vSm9iYmVyL0NsaWVudC80NDkwNjcxMg==",
+            "name" => "Jaina Proudmore",
+          },
+        ]
+      end
+
+      before do
+        allow_any_instance_of(JobberService).to(receive(:execute_paginated_query).and_return(clients_querry_result))
+
+        get :index
+      end
+
+      it { expect(response).to(have_http_status(:ok)) }
+
+      it "should return the list of clients" do
+        expect(result["clients"]).to(eq(clients_querry_result))
+      end
+    end
+
+    context "when query_result returns errors" do
+      let(:throttling_error) do
+        query_result = OpenStruct.new
+        query_result.original_hash = {
+          "errors" => [
+            {
+              "message" => "Throttled",
+              "extensions" => {
+                "code" => "THROTTLED",
+                "documentation" => "https://developer.getjobber.com/docs/build_with_jobber/api_rate_limits",
+              },
+            },
+          ],
+        }
+        query_result
+      end
+
+      before do
+        allow(JobberAppTemplateRailsApi::Client).to(receive(:query).and_return(throttling_error))
+
+        get :index
+      end
+
+      it { expect(response).to(have_http_status(:internal_server_error)) }
+
+      it { expect(result["message"]).to(eq("Exceptions::GraphQLQueryError: Throttled")) }
+    end
+  end
+end
--- spec/app/controllers/jobber_accounts_controller_spec.rb
+++ spec/app/controllers/jobber_accounts_controller_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe(JobberAccountsController, type: :controller) do
   let(:jobber_account) { create(:jobber_account) }
   let(:result) { JSON.parse(response.body) }
 
+  before do
+    allow(controller).to(receive(:jobber_account_id).and_return(jobber_account.jobber_id))
+  end
+
   describe "#jobber_account_name" do
-    before do
-      allow(controller).to(receive(:jobber_account_id).and_return(jobber_account.jobber_id))
-      get :jobber_account_name
-    end
+    before { get :jobber_account_name }
 
     it "should return 200 status code" do
-      expect(response).to(have_http_status(200))
+      expect(response).to(have_http_status(:ok))
     end
 
     it "should return the account name" do
